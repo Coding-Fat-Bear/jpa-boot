6b5f8748cce40d83c6b560a4b37baa22cd08cb49
Lijoe <Lijoe@LAPTOP-S8TJ8IDO>
2022-03-24 07:30:19 +0530

    version 1.2.7 timesheet workin,time calculation with break

--- src/main/java/com/agnie/RestAPI/ServiceImpl/TimesheetServiceImpl.java
+++ src/main/java/com/agnie/RestAPI/ServiceImpl/TimesheetServiceImpl.java
@@ -10,12 +10,8 @@ import com.agnie.RestAPI.Repository.TimesheetRepository;
 import com.agnie.RestAPI.Service.TimesheetService;
 import java.sql.Date;
 import java.sql.Time;
-import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.time.Period;
-import java.time.temporal.ChronoUnit;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -40,7 +36,6 @@ public class TimesheetServiceImpl implements TimesheetService{
        
         Optional<Timesheet> timesheet= tRepo.findById(Id);
         if(timesheet.isPresent()){
-            
             return timesheet.get();
         }
         throw new RuntimeException("There is no project for this ID "+Id);
@@ -51,23 +46,7 @@ public class TimesheetServiceImpl implements TimesheetService{
         if(timesheet.isPresent() )
             
         {
-             LocalDateTime d1 = LocalDateTime.of(1996, 1, 1, timesheet.get().getCheckin().getHours(),timesheet.get().getCheckin().getMinutes());
-          LocalDateTime d2 = LocalDateTime.of(1996, 1,1, timesheet.get().getCheckout().getHours(),timesheet.get().getCheckout().getMinutes());
-            LocalDateTime d3 = LocalDateTime.of(1996, 1,2, timesheet.get().getCheckout().getHours(),timesheet.get().getCheckout().getMinutes());
 
-        if (d1.isBefore(d2)) {
-            
-        }else{
-            d2 = d3;
-        }
-        Duration duration = Duration.between(d1, d2);
-        
-        long t = duration.toMinutes();
-        long hours = t / 60;
-        long minutes = t % 60;
-        String str1 = hours+"."+ minutes;
-        double d=Double.parseDouble(str1); 
-        timesheet.get().setTotalhours(d);
             return timesheet.get() ;
         }
        throw new IllegalStateException("There is no project for this ID "+tsdate+ " for your login Id");
@@ -82,26 +61,36 @@ public class TimesheetServiceImpl implements TimesheetService{
             System.out.println(timesheet.getCheckout());
            
         System.out.println(timesheet.getCheckin().getHours());
-            LocalDateTime d1 = LocalDateTime.of(1996, 1, 1, timesheet.getCheckin().getHours(),timesheet.getCheckin().getMinutes());
-          LocalDateTime d2 = LocalDateTime.of(1996, 1,1, timesheet.getCheckout().getHours(),timesheet.getCheckout().getMinutes());
-            LocalDateTime d3 = LocalDateTime.of(1996, 1,2, timesheet.getCheckout().getHours(),timesheet.getCheckout().getMinutes());
-
-//        LocalDateTime d2 = LocalDateTime.of(1996, 1, 1, 8,25); 
-//        LocalDateTime d3 = LocalDateTime.of(1996, 1, 2, 8,25);
-        if (d1.isBefore(d2)) {
-            
-        }else{
-            d2 = d3;
-        }
-        Duration duration = Duration.between(d1, d2);
+        Time tCheckin = timesheet.getCheckin();
+        Time tCheckout = timesheet.getCheckout();
         
-        long t = duration.toMinutes();
-        if(timesheet.getBtstart()==null)
-        {System.out.println("null");}
+        long t1 =  dateDifMin(tCheckin,tCheckout);
+        long t ;
+        if(timesheet.getBtstart()==null || timesheet.getBtend()==null)
+               {System.out.println("null");
+               t = t1;
+               }
+
+               else{
+                   System.out.println("not null");
+                   Time tBtstart = timesheet.getBtstart();
+                    Time tBtend = timesheet.getBtend();
+                    long t2 =  dateDifMin(tBtstart,tBtend);
+                    t= t1-t2;
+               }
+        
+        
         long hours = t / 60;
         long minutes = t % 60;
-        String str1 = hours+"."+ minutes;
+        String str1;
+            if(minutes <10){
+                str1 = hours+".0"+ minutes;
+            }
+            else{
+                str1 = hours+"."+ minutes;
+            }
         double d=Double.parseDouble(str1); 
+        System.out.println(d);
         timesheet.setTotalhours(d);
         return tRepo.save(timesheet);
     }
@@ -122,6 +111,59 @@ public class TimesheetServiceImpl implements TimesheetService{
         return tRepo.findByTsdate(tsdate);
     }
 
+   
     
+//    public long dateDifMin(Timesheet timesheet) {
+//
+//      LocalDateTime d1 = LocalDateTime.of(1996, 1, 1, timesheet.getCheckin().getHours(),timesheet.getCheckin().getMinutes());
+//      LocalDateTime d2 = LocalDateTime.of(1996, 1,1, timesheet.getCheckout().getHours(),timesheet.getCheckout().getMinutes());
+//      LocalDateTime d3 = LocalDateTime.of(1996, 1,2, timesheet.getCheckout().getHours(),timesheet.getCheckout().getMinutes());
+//
+//        if (d1.isBefore(d2)) {
+//
+//        }
+//        else{
+//            d2 = d3;
+//        }
+//    Duration duration = Duration.between(d1, d2);
+//    long t = duration.toMinutes();
+//        
+//       if(timesheet.getBtstart()==null || timesheet.getBtend()==null)
+//        {System.out.println("null");
+//        
+//        }
+//        
+//        else{
+//            System.out.println("not null");
+//            
+//        }
+//      
+//      
+//        
+//        return t;
+//                
+//    }
+    
+    
+    public long dateDifMin(Time t1,Time t2) {
+
+      LocalDateTime d1 = LocalDateTime.of(1996, 1, 1, t1.getHours(),t1.getMinutes());
+      LocalDateTime d2 = LocalDateTime.of(1996, 1,1, t2.getHours(),t2.getMinutes());
+      LocalDateTime d3 = LocalDateTime.of(1996, 1,2, t2.getHours(),t2.getMinutes());
+        System.out.println(d1);
+        System.out.println(d2);
+        if (d1.isBefore(d2)) {
+
+        }
+        else{
+            d2 = d3;
+        }
+    Duration duration = Duration.between(d1, d2);
+    long t = duration.toMinutes();
+        
+        return t;
+                
+    }
+        
 }
 

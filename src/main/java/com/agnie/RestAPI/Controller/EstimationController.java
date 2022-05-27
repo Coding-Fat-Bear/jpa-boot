3a603d08f8ca8aef36f6d4af6b61626d8722fc46
Lijoe <Lijoe@LAPTOP-S8TJ8IDO>
2022-05-27 08:52:02 +0530

    ver 1.3

---
+++ src/main/java/com/agnie/RestAPI/Controller/EstimationController.java
@@ -0,0 +1,71 @@
+    /*
+    * To change this license header, choose License Headers in Project Properties.
+    * To change this template file, choose Tools | Templates
+    * and open the template in the editor.
+    */
+    package com.agnie.RestAPI.Controller;
+
+    import com.agnie.RestAPI.Model.Estimation;
+    import com.agnie.RestAPI.Service.EstimationService;
+    import java.util.List;
+    import org.springframework.beans.factory.annotation.Autowired;
+    import org.springframework.web.bind.annotation.CrossOrigin;
+    import org.springframework.web.bind.annotation.DeleteMapping;
+    import org.springframework.web.bind.annotation.GetMapping;
+    import org.springframework.web.bind.annotation.PathVariable;
+    import org.springframework.web.bind.annotation.PostMapping;
+    import org.springframework.web.bind.annotation.PutMapping;
+    import org.springframework.web.bind.annotation.RequestBody;
+    import org.springframework.web.bind.annotation.ResponseBody;
+    import org.springframework.web.bind.annotation.RestController;
+
+    /**
+    *
+    * @author Admin Mahesh
+    */
+    //@CrossOrigin
+    //mark class as Controller 
+    @RestController
+    public class EstimationController {
+
+    //autowire the EstimationService class
+    @Autowired
+     EstimationService estService;
+
+    //creating a get mapping that retrieves all the estimations detail from the database
+    @GetMapping("/Estimations")
+    public @ResponseBody List<Estimation> getAllEstimations()   
+    {  
+     return estService.getAllEstimations();  
+
+    } 
+
+     // creating a get mapping that retrieves the detail of a specific Estimation  
+    @GetMapping("/Estimations/{estmno}")  
+    public Estimation getEstimations(@PathVariable("estmno") long estmno)   
+    {  
+    return estService.getEstimationsById(estmno);  
+    } 
+    
+         //creating post mapping that post the estimation detail in the database  
+    @PostMapping("/createEstimation")  
+    public Estimation saveEstimation(@RequestBody Estimation estimation)   
+    {  
+    return estService.saveEstimation(estimation);    
+    }  
+    
+        //creating a delete mapping that deletes a specified estimation  
+    @DeleteMapping("/Estimations/{estmno}")  
+    public void deleteEstimation(@PathVariable("estmno") long estmno)   
+    {  
+    estService.delete(estmno);  
+    } 
+    
+        //creating put mapping that updates the estimation detail   
+    @PutMapping("/editEstimation/{estmno}")  
+    public Estimation updateEstimation(@PathVariable("estmno") long estmno, @RequestBody Estimation estimation)   
+    {  
+    return estService.updateEstimation(estmno, estimation);  
+    }
+
+    }
---
+++ src/main/java/com/agnie/RestAPI/Model/Inquiry.java
@@ -0,0 +1,92 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.agnie.RestAPI.Model;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ *
+ * @author Admin Mahesh
+ */
+
+    //mark class as an Entity
+    @Entity
+
+    // Getter and Setter Methods
+    @Getter
+    @Setter
+    @ToString
+
+    //defining Table name in Class
+    @Table(name = "Inquiry")
+
+public class Inquiry implements Serializable {
+            //Defining Estimation no as primary key 
+        @Id
+        @Column
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long inqno;
+        @Column
+        private String ccod;
+        @Column
+        private Long empno;
+        @Column
+        private String assnper;
+        @Column
+        private String bizid;
+        @Column
+        private String modul;
+        @Column
+        private String lngid;
+        @Column
+        private String frmdt;
+        @Column
+        private String todt;
+        @Column
+        private String posbper;
+        @Column
+        private String prjdes;
+        @Column
+        private String prjsts;
+        @Column
+        private String phaseid;
+        @Column
+        private double estmamt;
+        @Column
+        private String curr;
+        @Column
+        private double tax;
+        @Column
+        private String unit;
+        @Column
+        private int qty;
+        @Column
+        private double rate;
+        @Column 
+        private String custno;
+        @Column
+        private String eusrno;
+        @Column
+        private String custinfo;
+        @Column 
+        private String remarks;
+        @Column
+        private String actflg;
+        @Column
+        private String credt;
+        @Column
+        private String cretim;
+    
+}
